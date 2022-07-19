8d2080b37544dbae214acf9f5dd714f8e9874336
Tugba Yalcinkaya <tugbayalcinkaya3437@gmail.com>
2022-07-19 22:24:03 +0300

    project created

---
+++ FirstProjects/FirstProjects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProjects
+//
+//  Created by Tuğba/Kübra on 19.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
